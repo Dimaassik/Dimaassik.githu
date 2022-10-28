6800800889664d79a275dea6ef3fdd877102932a
Dimaassik <116507628+Dimaassik@users.noreply.github.com>
2022-10-29 00:17:10 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ body.appendChild(div1);
 const inp = document.createElement('input');
 div1.appendChild(inp);
 const h1 = document.createElement('h1');
-h1.textContent = 'Введiть те що хочете запитати в кулi';
+h1.textContent = 'Введіть те, що хочете запитати в кулі';
 div1.appendChild(h1);
 h1.insertAdjacentElement('afterend',inp);
 const br1 = document.createElement('br');
@@ -25,7 +25,7 @@ btn.addEventListener('click', () => {
 	if(st == -1){
 		h2.textContent = 'Не бачу питання'
 	}else{
-var arr = ['Нi', 'Нi в якому разі', 'Гадаю що нi','Складно вiдповiсти','Можливо','Так','Однозначно так','Духи нейтральнi','Без сумнiвiв','Шансiв мало','Смутно'];
+var arr = ['Ні', 'Ні в якому разі', 'Гадаю що ні','Складно відповісти','Можливо','Так','Однозначно так','Духи нейтральні','Без сумнівів','Шансів мало','Смутно'];
 var r = Math.floor(Math.random() * 11);
 	h2.textContent = String(arr[r]);}
 });
